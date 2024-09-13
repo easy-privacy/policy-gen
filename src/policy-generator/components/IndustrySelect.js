23c4415f74d054ad93fb010ecd8bda496d0580ad
Amrit Kochar <amritkochar.007@gmail.com>
2024-09-13 18:55:42 +0530

    update with some more industries

--- src/policy-generator/components/IndustrySelect.js
+++ src/policy-generator/components/IndustrySelect.js
@@ -10,8 +10,16 @@ function IndustrySelect({ onChange }) {
         className="w-full rounded-md p-2 border border-gray-300"
       >
         <option value="">Select industry</option>
-        <option value="hospitality">Hospitality</option>
-        {/* Add more industries here as needed */}
+        <option value="Restaurants">Restaurants</option>
+        <option value="Hotel">Hotel</option>
+        <option value="Hospitals_and_Healthcare">Hospitals and Healthcare</option>
+        <option value="Educational_Institutions">Educational Institutions</option>
+        <option value="Ecommerce">Ecommerce</option>
+        <option value="Charity">Charity</option>
+        <option value="Social_Media">Social Media</option>
+        <option value="Tech_Manufacturing">Tech Manufacturing</option>
+        <option value="Manufacturing">Manufacturing</option>
+        <option value="Transportation_and_Warehousing">Transportation and Warehousing</option>
       </select>
     </div>
   );
